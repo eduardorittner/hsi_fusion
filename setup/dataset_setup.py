02b13a528c8c9df25555623ec3cd72216b6ad087
eduardorittner <eduardorittnerc@gmail.com>
2024-05-14 15:50:44 +0000

    Finish dataset setup

--- setup/dataset_setup.py
+++ setup/dataset_setup.py
@@ -9,9 +9,24 @@ import numpy as np
 import h5py
 import argparse
 from os import path
+import os
 from tqdm import tqdm
 
+
 def save2np(file, dest_path):
+    rgb_in_path = path.join(dest_path, "rgb_in")
+    msi_in_path = path.join(dest_path, "msi_in")
+    msi_out_path_path = path.join(dest_path, "msi_out_path")
+
+    if not path.isdir(rgb_in_path):
+        os.mkdir(rgb_in_path)
+
+    if not path.isdir(msi_in_path):
+        os.mkdir(msi_in_path)
+
+    if not path.isdir(msi_out_path):
+        os.mkdir(msi_out_path)
+
     with h5py.File(file) as f:
         keys = list(f.keys())
 
@@ -19,15 +34,50 @@ def save2np(file, dest_path):
             arr = f[key][()]
             rgb_in = arr[:4].T
             msi_out_vis = arr[4 : 4 + 31]
-            msi_out_nir = arr[31 + 4 + 1:] # Skip 35th band since it's a duplicate of 36
+            msi_out_nir = arr[
+                31 + 4 + 1 :
+            ]  # Skip 35th band since it's a duplicate of 36
             msi_out = np.concatenate((msi_out_vis, msi_out_nir), axis=0).T
 
-            msi_in = msi_out[:, :: 4, :: 4]
-            np.save(path.join(dest_path, key), arr)
+            msi_in = msi_out[:, ::4, ::4]
 
+            np.save(path.join(rgb_in_path, key), rgb_in)
+            np.save(path.join(msi_in_path, key), msi_in)
+            np.save(path.join(msi_out_path, key), msi_out)
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Setup initial dataset with different files for rgb_in, msi_in and msi_out from .hdf file"
+    )
 
+    parser.add_argument(
+        "train_source_file",
+        type=str,
+        help="Path to train .hdf file",
+        default=None,
+    )
 
-with h5py.File(
+    parser.add_argument(
+        "val_source_file",
+        type=str,
+        help="Path to val.hdf file",
+        default=None,
+    )
+
+    parser.add_argument(
+        "dest_dir",
+        type=str,
+        help="Path to directories where dataset will be stored",
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    print(
+        f"Loading files from train file: {args.train_source_file} into: {args.dest_dir}"
+    )
+    save2np(args.train_source_file, args.dest_dir)
+
+    print(f"Loading files from val file: {args.val_source_file} into: {args.dest_dir}")
+    save2np(args.val_source_file, args.dest_dir)
