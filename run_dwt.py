91794234a81c8c25f49ee8a768eb55531fb68155
eduardorittner <eduardorittnerc@gmail.com>
2024-05-22 23:57:33 +0000

    f string

--- run_dwt.py
+++ run_dwt.py
@@ -220,7 +220,7 @@ if __name__ == "__main__":
         deviation_path = join(args.results, "deviation")
         np.save(mean_path, mean)
         np.save(deviation_path, deviation)
-        print(r"results saved in {dir}")
+        print(f"results saved in {dir}")
         exit(0)
 
     if args.suite is not None:
