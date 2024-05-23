c0c5eb4f72b4d89251f4fa16a51e5821bd75828c
eduardorittner <eduardorittnerc@gmail.com>
2024-05-23 17:47:49 +0000

    2ddwt

--- dwt/dwt.py
+++ dwt/dwt.py
@@ -3,6 +3,53 @@ import numpy as np
 from typing import List, Callable
 
 
+class DWT2D_coeffs(object):
+    def __init__(self, image: np.ndarray, wavelet: type[List[str] | str], level: int):
+        self.wavelet = wavelet
+        self.level = level
+        self.coeffs, self.slices, self.shapes = pywt.ravel_coeffs(
+            pywt.wavedecn(image, wavelet=wavelet, level=level)
+        )
+        self.coeffs = np.array(self.coeffs)
+
+    def copy_coeffs(self):
+        return np.copy(self.coeffs)
+
+    def coeffs_approx(self):
+        return (self.coeffs[self.slices[0]], self.slices[0])
+
+    def coeffs_detail(self):
+        results = []
+
+        for i in range(self.level):
+            results.append(
+                (self.coeffs[self.slices[i + 1]["dd"]], self.slices[i + 1]["dd"])
+            )
+            results.append(
+                (self.coeffs[self.slices[i + 1]["ad"]], self.slices[i + 1]["ad"])
+            )
+            results.append(
+                (self.coeffs[self.slices[i + 1]["da"]], self.slices[i + 1]["da"])
+            )
+
+        return results
+
+
+def fuse_approx_2d(rgb_in: DWT2D_coeffs, msi_in: DWT2D_coeffs, fused: np.ndarray):
+    rgb = rgb_in.coeffs_approx()
+    msi = msi_in.coeffs_approx()
+
+    start, stop = rgb[1].start, rgb[1].stop
+
+    fused[start:stop] = (rgb[0][start:stop] + msi[0][start:stop]) / 2
+
+
+def fuse_detail_2d(rgb_in: DWT2D_coeffs, msi_in: DWT2D_coeffs, fused: np.ndarray):
+    rgb = rgb_in.coeffs_detail()
+    start, stop = rgb[1].start, rgb[1].stop
+    fused[start:stop] = rgb[start:stop]
+
+
 class DWT_coeffs(object):
     def __init__(self, image: np.ndarray, wavelet: type[List[str] | str], level: int):
         self.wavelet = wavelet
@@ -112,10 +159,22 @@ def fuse_2dDWT(
     if transform is not None:
         rgb_in, msi_in, _ = transform(rgb_in, msi_in, None)
 
-    rgb_coeffs = DWT_coeffs(rgb_in, wavelet, level)
-    msi_coeffs = DWT_coeffs(msi_in, wavelet, level)
+    results = np.array(rgb_in.shape)
 
-    return
+    for band in range(rgb_in.shape[2]):
+        rgb_coeffs = DWT2D_coeffs(rgb_in[:, :, band], wavelet, level)
+        msi_coeffs = DWT2D_coeffs(msi_in[:, :, band], wavelet, level)
+
+        fused_coeffs = rgb_coeffs.copy_coeffs()
+        fuse_approx_2d(rgb_coeffs, msi_coeffs, fused_coeffs)
+        fuse_detail_2d(rgb_coeffs, msi_coeffs, fused_coeffs)
+
+        results[:, :, band] = pywt.waverec2(
+            pywt.unravel_coeffs(fused_coeffs, rgb_coeffs.slices, rgb_coeffs.shapes),
+            wavelet=wavelet,
+        )
+
+    return results
 
 
 def fuse_3dDWT(
