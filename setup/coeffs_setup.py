c9336d0540457fea60244ecc5aea7b5c79d8bc87
erittner <eduardorittnerc@gmail.com>
2024-06-05 10:49:52 -0300

    things

--- setup/coeffs_setup.py
+++ setup/coeffs_setup.py
@@ -1,17 +1,25 @@
 import numpy as np
 import glob
+import time
 from os import path
 import pywt
+from tqdm import tqdm
+
+
+def name(file):
+    return file.split("/")[-1].split(".")[0]
 
 base_path = "/home/eduardo/data/msi_out/"
+dest_path = "/home/eduardo/data/coeffs/"
 
 files = sorted(glob.glob(path.join(base_path, "*.npy")))
 
-for file in files:
+for i in tqdm(range(len(files))):
+    file = files[i]
     img = np.load(file)
-    coeffs, slices, shapes = pywt.ravel_coeffs(pywt.wavedecn(img, "coif1", level=2))
-    print(coeffs.max())
-    print(coeffs.min())
 
-    pywt.waverecn(pywt.unravel_coeffs(coeffs, slices, shapes, output_format="wavedecn"))
-    exit()
+    img = img[::2, ::2, :]
+
+    coeffs, slices, shapes = pywt.ravel_coeffs(pywt.wavedecn(img, "coif1", level=2))
+
+    np.save(path.join(dest_path, name(file)), coeffs)
