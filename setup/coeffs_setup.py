13bd8c6d6439dffa2478d656c1a7bbd6bcbf88a9
eduardorittner <eduardorittnerc@gmail.com>
2024-07-16 20:29:44 +0000

    iupi

--- setup/coeffs_setup.py
+++ setup/coeffs_setup.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 def name(file):
     return file.split("/")[-1].split(".")[0]
 
+
 base_path = "/home/eduardo/data/msi_out/"
 dest_path = "/home/eduardo/data/coeffs/"
 
@@ -18,8 +19,8 @@ for i in tqdm(range(len(files))):
     file = files[i]
     img = np.load(file)
 
-    img = img[::2, ::2, :]
+    img = img[::4, ::4, :]  # Resolução de 256x256
 
-    coeffs, slices, shapes = pywt.ravel_coeffs(pywt.wavedecn(img, "coif1", level=2))
+    coeffs, slices = pywt.coeffs_to_array(pywt.wavedecn(img, "db2", level=2))
 
     np.save(path.join(dest_path, name(file)), coeffs)
